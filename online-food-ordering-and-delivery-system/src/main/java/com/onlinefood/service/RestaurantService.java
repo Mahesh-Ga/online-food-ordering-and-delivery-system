f5a3a6e7e553f51bb1245fdf52f4cd6747796123
lalitgupta01 <gupta.lalit199@gmail.com>
2023-08-19 08:47:07 +0530

    admin added

--- online-food-ordering-and-delivery-system/src/main/java/com/onlinefood/service/RestaurantService.java
+++ online-food-ordering-and-delivery-system/src/main/java/com/onlinefood/service/RestaurantService.java
@@ -1,5 +1,25 @@
 package com.onlinefood.service;
 
-public interface RestaurantService {
+import java.util.List;
 
+import com.onlinefood.dto.ApiResponse;
+import com.onlinefood.entities.Restaurant;
+
+public interface RestaurantService {
+	
+//	public ApiResponse addRestaurant(Restaurant res);
+//
+//	public List<RestaurantDto> pendingRestaurantRequests();
+//
+//	public ApiResponse approveRestaurant(Long id);
+//
+//	public ApiResponse removeRestaurant(Long id);
+//
+//	public ApiResponse rejectRestaurant(Long id);
+//
+//	public List<RestaurantDto> getAllActiveRestaurants();
+
+	
+	
+	
 }
--- online-food-ordering-and-delivery-system/src/main/java/com/onlinefood/service/RestaurantServiceImpl.java
+++ online-food-ordering-and-delivery-system/src/main/java/com/onlinefood/service/RestaurantServiceImpl.java
@@ -1,5 +1,76 @@
 package com.onlinefood.service;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.onlinefood.dto.ApiResponse;
+import com.onlinefood.entities.Restaurant;
+import com.onlinefood.repository.RestaurantRepo;
+
+@Service
+@Transactional
 public class RestaurantServiceImpl implements RestaurantService {
 
+
+//	@Autowired
+//	RestaurantRepo resDao;
+//
+//	@Autowired
+//	ModelMapper mapper;
+//	
+//	@Override
+//	public ApiResponse addRestaurant(Restaurant res) {
+//
+//		resDao.save(res);
+//		return new ApiResponse("sucessfully added");
+//
+//	}
+//
+//	@Override
+//	public List<RestaurantDto> pendingRestaurantRequests() {
+//
+//		return resDao.getPendingRestaurants().stream()
+//				.map(res->mapper.map(res, RestaurantDto.class))
+//				.collect(Collectors.toList());
+//		
+//	}
+//
+//	@Override
+//	public List<RestaurantDto> getAllActiveRestaurants() {
+//		return resDao.getAllActiveRestauraants().stream()
+//				.map(res->mapper.map(res, RestaurantDto.class))
+//				.collect(Collectors.toList());
+//		
+//	}
+//
+//	@Override
+//	public ApiResponse approveRestaurant(Long id) {
+//		Restaurant res = resDao.findById(id).orElseThrow();
+//		res.setApproved(true);
+//		return new ApiResponse("sucessfully approved");
+//	}
+//
+//	@Override
+//	public ApiResponse rejectRestaurant(Long id) {
+//		Restaurant res = resDao.findById(id).orElseThrow();
+//		if (!res.isApproved()) {
+//			resDao.delete(res);
+//			return new ApiResponse("sucessfully rejected");
+//		} else
+//			return new ApiResponse("failed to reject");
+//	}
+//
+//	@Override
+//	public ApiResponse removeRestaurant(Long id) {
+//		Restaurant res = resDao.findById(id).orElseThrow();
+//		res.setDeleted(true);
+//		return new ApiResponse("sucessfully removed");
+//	}
+
+	
+	
+	
+	
 }
