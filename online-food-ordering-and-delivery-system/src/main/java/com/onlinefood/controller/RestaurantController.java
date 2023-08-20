c45342f34dbd9031bde5c900d869d8a2f72cbe0a
Kalpesh <kalpeshbw01@gmail.com>
2023-08-20 16:32:17 +0530

    Restaurant Controller with validations and image handling

--- online-food-ordering-and-delivery-system/src/main/java/com/onlinefood/controller/RestaurantController.java
+++ online-food-ordering-and-delivery-system/src/main/java/com/onlinefood/controller/RestaurantController.java
@@ -1,8 +1,13 @@
 package com.onlinefood.controller;
 
+import java.io.IOException;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,13 +16,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.onlinefood.dto.ApiResponse;
 import com.onlinefood.dto.GetMenuDTO;
+import com.onlinefood.dto.OrderDTOforRestaurant;
 import com.onlinefood.dto.RestaurantNewMenuDTO;
 import com.onlinefood.dto.RestaurantResponseDTO;
 import com.onlinefood.dto.RestaurantSignupDTO;
+import com.onlinefood.entities.Order;
 import com.onlinefood.service.RestaurantService;
 
 
@@ -29,49 +38,68 @@ public class RestaurantController {
 	RestaurantService restaurantService;
 	
 	@PostMapping
-	public ApiResponse addRestaurant(@RequestBody RestaurantSignupDTO restaurant) {
-		return restaurantService.addRestaurant(restaurant);
+	public  ResponseEntity<?> addRestaurant(@RequestBody @Valid RestaurantSignupDTO restaurant) {
+		return ResponseEntity.ok(restaurantService.addRestaurant(restaurant));
 	}
 	
 	@PostMapping("/addmenu/{resId}")
-	public ApiResponse addMenu(@PathVariable Long resId,@RequestBody RestaurantNewMenuDTO menu)
+	public ResponseEntity<?> addMenu(@PathVariable Long resId,@RequestBody @Valid RestaurantNewMenuDTO menu)
     {
-		return restaurantService.addMenu(resId, menu);
+		return ResponseEntity.ok(restaurantService.addMenu(resId, menu));
 		
 	}
 	@GetMapping("/menu")
-	public List<GetMenuDTO> getMenu(){
+	public ResponseEntity<?> getMenu(){
 		List<GetMenuDTO> allMenuList=restaurantService.getAllMenu();
 		
-		return allMenuList;
+		return new ResponseEntity<>(allMenuList,HttpStatus.OK);
 	}
 	
 	@GetMapping("/menubyResId/{resId}")
-	public List<GetMenuDTO> getMenu(@PathVariable Long resId){
+	public ResponseEntity<?> getMenu(@PathVariable Long resId){
 		List<GetMenuDTO> menuList=restaurantService.getAllMenuByRestaurantId(resId);
 		
-		return menuList;
+		return new ResponseEntity<>(menuList,HttpStatus.OK);
 	}
 	
 	@GetMapping("/menu/{menuId}")
-	public GetMenuDTO getMenuById(@PathVariable Long menuId){
+	public ResponseEntity<?> getMenuById(@PathVariable Long menuId){
 		//System.out.println(restaurantService.getMenuById(menuId).getName());
 		GetMenuDTO menu=restaurantService.getMenuById(menuId);	
 		//System.out.println(menu.getName());
-		return menu;
+		return new ResponseEntity<>(menu,HttpStatus.OK);
 	}
 	
 	
 	@PutMapping("/menu/{menuId}")
-	public ApiResponse updateMenu(@PathVariable Long menuId,@RequestBody RestaurantNewMenuDTO menu) {
-		return restaurantService.updateMenu(menu, menuId);
+	public ResponseEntity<?> updateMenu(@PathVariable Long menuId,@RequestBody @Valid RestaurantNewMenuDTO menu) {
+		return ResponseEntity.ok(restaurantService.updateMenu(menu, menuId));
 	}
 
 	
 	@DeleteMapping("/menu/{menuId}")
-	public ApiResponse updateMenu(@PathVariable Long menuId) {
-		return restaurantService.removeMenu(menuId);
+	public ResponseEntity<?> removeMenu(@PathVariable Long menuId) {
+		return ResponseEntity.ok(restaurantService.removeMenu(menuId));
 	}
 	
+	@GetMapping("/orderByRestaurant/{resId}")
+	public ResponseEntity<?> getPendingOrders(@PathVariable Long resId){
+		OrderDTOforRestaurant myPendingOrder=restaurantService.getMyPendingOrder(resId);
+		return  ResponseEntity.ok(myPendingOrder);
+	}
+	@GetMapping("/confirmOrder/{orderId}")
+	public ResponseEntity<?> confirmOrder(@PathVariable Long orderId){
+		return ResponseEntity.ok(restaurantService.changeOrderStatus(orderId));
+	}
+	@GetMapping("/orderReadyForPickup/{orderId}")
+	public ResponseEntity<?> OrderReadyForPickup(@PathVariable Long orderId){
+		return ResponseEntity.ok(restaurantService.OrderReadyForPickUp(orderId));
+	}
+	@PostMapping(value = "/images/{menuId}", consumes = "multipart/form-data")
+	public ResponseEntity<?> uploadMenuImage(@PathVariable Long menuId, @RequestParam MultipartFile imageFile)
+			throws IOException {
+		System.out.println("in upload img " + menuId);
+		return ResponseEntity.status(HttpStatus.CREATED).body(restaurantService.uploadMenuImage(menuId, imageFile));
+	}
 	
 }
