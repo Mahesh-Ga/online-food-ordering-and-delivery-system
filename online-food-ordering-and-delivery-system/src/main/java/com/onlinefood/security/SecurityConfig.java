2c594aa9a0062e3dd0e3d972e6e76272104baafd
mahesh <maheshsg2016@gmail.com>
2023-08-22 01:36:03 +0530

    completed backend

--- online-food-ordering-and-delivery-system/src/main/java/com/onlinefood/security/SecurityConfig.java
+++ online-food-ordering-and-delivery-system/src/main/java/com/onlinefood/security/SecurityConfig.java
@@ -3,18 +3,17 @@ package com.onlinefood.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.onlinefood.entities.RoleType;
 
 @EnableWebSecurity
 @Configuration
@@ -31,18 +30,19 @@ public class SecurityConfig {
 				.and()
 				.csrf().disable()
 				.authorizeRequests()
-				.antMatchers
-				(
-				"/restaurant/menu", 
+				.antMatchers        // Applies to all HTTP methods(GET,PUT,POST,DELETE)
+				(HttpMethod.GET,"/restaurant/menu/*").permitAll()
+				.antMatchers( 
 				"/customer/signUp",
 				"/user/signin", 
-				"/restaurant",
-				"/restaurant/menubyResId*", 
-				"/restaurant/menu*",
+				"/restaurant/menubyResId/*", 
+				"/restaurant/menu",
 				"/swagger*/**",
-				"/v*/api-docs/**").permitAll()
-				.antMatchers("/customer/**").hasRole("CUSTOMER")
+				"/v*/api-docs/**").permitAll()  
+				.antMatchers("/customer/**","/cart/**").hasRole("CUSTOMER")
 				.antMatchers("/admin/**").hasRole("ADMIN")
+				.antMatchers("/restaurant/**").hasRole("RESTAURANT")
+				.antMatchers("/delivery/**").hasRole("DELIVERY_PARTNER")
 				.anyRequest().authenticated()
 				.and()
 				.sessionManagement()
